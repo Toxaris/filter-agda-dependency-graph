b7f77c51683bcf308f7a2581f12fbdfbdc37a42a
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2013-09-26 18:52:12 +0200

    Color-code module prefixes.

--- filter-agda-dependency-graph.hs
+++ filter-agda-dependency-graph.hs
@@ -1,35 +1,50 @@
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns, ParallelListComp #-}
 module Main where
 
-import Prelude (Num ((-)))
+import Prelude (Num ((+), (-)))
 
 import Control.Monad (return, filterM)
 
 import Data.Array ((!))
-import Data.Bool (Bool (False, True), (&&), (||))
-import Data.Eq (Eq ((==)))
+import Data.Bool (Bool (False, True), (&&), (||), not)
+import Data.Eq (Eq ((==), (/=)))
 import Data.Foldable (toList)
-import Data.Function ((.))
+import Data.Function ((.), ($))
 import Data.Graph (graphFromEdges, path)
 import Data.GraphViz (parseDotGraph, printDotGraph)
 import Data.GraphViz.Attributes.Complete
-  ( Attribute (Label)
+  ( Attribute (Color, Label, Style, Shape)
+  , Color (BrewerColor)
   , Label (StrLabel)
+  , Shape (BoxShape)
+  , StyleItem (SItem)
+  , StyleName (Filled, Invisible)
+  , WeightedColor (WC)
+  )
+import Data.GraphViz.Attributes.Colors.Brewer
+  ( BrewerColor (BC)
+  , BrewerName (Accent)
+  , BrewerScheme (BScheme)
   )
 import Data.GraphViz.Types.Generalised
   ( DotGraph (DotGraph), graphStatements
   , DotNode (DotNode), nodeID, nodeAttributes
-  , DotEdge (DotEdge), fromNode, toNode
-  , DotStatement (DN, DE)
-  , GraphID
+  , DotEdge (DotEdge), fromNode, toNode, edgeAttributes
+  , DotSubGraph (DotSG), isCluster, subGraphID, subGraphStmts
+  , DotStatement (DN, DE, SG)
+  , GraphID (Str)
   )
-import Data.List (map, null, drop, elem, notElem)
-import Data.Maybe (fromJust)
-import Data.Map as Map (fromList, size, lookup)
+import Data.Int (Int)
+import Data.List (map, null, drop, elem, notElem, sortBy, genericLength, take, (++))
+import Data.Maybe (fromJust, Maybe (Just, Nothing))
+import qualified Data.Map as Map (toList, fromList, fromListWith, size, lookup)
+import Data.Ord (Down (Down), comparing)
 import Data.Sequence as Seq (fromList)
 import Data.Set as Set (fromList, member)
-import Data.Text.Lazy (Text, splitOn, pack, unpack)
+import Data.Text.Lazy (Text, splitOn, pack, unpack, takeWhile, break)
+import qualified Data.Text.Lazy as Text (null, drop)
 import Data.Text.Lazy.IO (getContents, putStrLn)
+import Data.Tuple (fst, snd)
 
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
@@ -65,6 +80,24 @@ processGraph srcpath dotgraph@DotGraph {graphStatements} = do
         includes srcpath modulePath
   modules <- filterM decide modules
 
+  -- the list of module prefixes, sorted by usage count
+  let prefixes = take 8 $ map fst $ sortBy (comparing (Down . snd)) $ Map.toList $ Map.fromListWith (+)
+        [ (prefix, 1)
+        | (nodeID, label) <- modules
+        , let (prefix, rest) = break ('.' ==) label
+        , not (Text.null rest)
+        ]
+
+  -- the map of module prefixes to their indexes in the prefixes list
+  let prefixColor = Map.fromList
+        [ (prefix, color)
+        | prefix <- prefixes
+        | color <- [1..]
+        ]
+
+  -- the highest color number we need
+  let colors = genericLength prefixes
+
   -- the set of module ids we want to keep, with indexes
   let keep = Set.fromList [nodeID | (nodeID, label) <- modules]
 
@@ -80,9 +113,60 @@ processGraph srcpath dotgraph@DotGraph {graphStatements} = do
                 ]
         ]
 
+  -- color encoding
+  let encodeColor color colors =
+        Color [WC (BrewerColor (BC (BScheme Accent colors) color)) Nothing]
+
+  -- color-code the module prefix
+  let
+    adjustNode (DN (node@DotNode {nodeID, nodeAttributes})) =
+      DN (node {nodeAttributes = adjustAttributes nodeAttributes})
+    adjustNode x = x
+
+    adjustAttributes attrs = [result | attr <- attrs, result <- adjustAttribute attr]
+
+    adjustAttribute (Label (StrLabel label)) =
+      let (prefix, rest) = break ('.' ==) label
+      in case Map.lookup prefix prefixColor of
+        Just color ->
+          [ Label (StrLabel (Text.drop 1 rest))
+          , encodeColor color colors
+          , Style [SItem Filled []]
+          ]
+        Nothing ->
+          [ Label (StrLabel label) ]
+    adjustAttribute attr = [attr]
+
+  -- the legend
+  let legend = SG DotSG
+        { isCluster = False
+        , subGraphID = Nothing
+        , subGraphStmts = Seq.fromList $
+            [ DN DotNode
+                   { nodeID = Str prefix
+                   , nodeAttributes =
+                       [ Label (StrLabel prefix)
+                       , Shape BoxShape
+                       , encodeColor color colors
+                       , Style [SItem Filled []]
+                       ]
+                   }
+            | prefix <- prefixes
+            | color <- [1..]
+            ] ++
+            [ DE DotEdge
+                   { fromNode = Str fromPrefix
+                   , toNode = Str toPrefix
+                   , edgeAttributes = [Style [SItem Invisible []]]
+                   }
+            | fromPrefix <- prefixes
+            | toPrefix <- drop 1 prefixes
+            ]
+        }
+
   -- the filtered graph
-  return dotgraph {graphStatements = Seq.fromList
-    [ statement
+  return dotgraph {graphStatements = Seq.fromList $ legend :
+    [ adjustNode statement
     | statement <- toList graphStatements
     , case statement of
         DN DotNode {nodeID} -> nodeID `member` keep
